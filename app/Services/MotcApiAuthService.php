414362f871b09aae9b95aafc244e359a4b4bc4f4
ivankkday <ivan.huang@kkday.com>
2022-06-29 13:18:22 +0800

    temp

---
+++ app/Services/MotcApiAuthService.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Services;
+
+use App\Helpers\RequestHelper;
+
+class MotcApiAuthService{
+    const MOTC_API_URL="https://ptx.transportdata.tw/MOTC/";
+    const MOTC_ID="449486a53acf442181ec5aa06db7c2f9";
+    const MOTC_KEY='EJs6O1ss8BTkgqR11mIQAw9OAPg';
+
+    public function test(){
+       
+        $params['request_method'] = 'GET';
+        $params['uri'] = self::MOTC_API_URL.'/v2/Rail/THSR/DailyTimetable/TrainDates';
+        $params['options'] = [
+            "headers" => $this->getHeaders(),
+            "http_errors" => false
+            ];
+        
+        $response = RequestHelper::sendRequest($params);
+        echo "\n".$response->getBody();
+        // return $response;
+    }
+    public function authorize($xdate){
+        return 'hmac username="'.self::MOTC_ID.'",algorithm="hmac-sha1",headers="x-date",signature="'.$this->signature($xdate).'"';
+    }
+    public function signature(){
+        $xdate = "x-date:".$this->getXDate();
+        return base64_encode(hash_hmac('sha1', "x-date:".$xdate, self::MOTC_KEY, true));
+    }
+
+    public function getHeaders(){
+        $xdate = $this->getXDate();
+        return [
+            'Authorization' => $this->authorize($xdate),
+            'x-date' => $xdate,
+            'Accept' => 'application/json',
+            'Accept-Encoding' => 'gzip, deflate'
+        ];
+
+    }
+
+    public function getXDate(){
+        return date('D, d M Y H:i:s \G\M\T');
+    }
+}
